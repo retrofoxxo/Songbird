67d7bed6a3f2b13d2f88043683cb5cf65deba314
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-24 16:42:55 +0100

    Add search

--- users/12345/likes.php
+++ users/12345/likes.php
@@ -1,5 +1,4 @@
 <?php
 header('Content-Type: application/json');
-header('Pagination: 1');
 ?>
-[{"title": "test", "id": 123456, "facebook_id":"122102047148149148", "description": "testing photo's :>", "comment_count": 1, "like_count": 1, "liked": 0, "cover_image": {"src": "/test.png", "height": 100}, "artist":{"artist_name": "zilch", "id": 123456}, "type": "cover_image", "url": "/test.png"}]
+[]
