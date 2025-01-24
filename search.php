1d54c73b06196027ca4019d55b4762503540b884
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-24 19:48:01 +0100

    add profile images to artist results

--- search.php
+++ search.php
@@ -2,4 +2,4 @@
 header('Content-Type: application/json');
 header('Pagination: 1');
 ?>
-[{"id":"12345", "facebook_id":"122102047148149148", "profile_picture": "/test.png", "artist_name":"Retrofoxxo", "biography":{"text":"I'm a fox", "providerUrl":"http://zilchfox.com", "provider":"Zilch, Fox and Angel", "site":"zilchfox.com", "licenseUrl":"test.com", "license":"fuck"}, "tracks":[{"title":"Foxxo & Friends", "purchase_url":"/purchase", "cover_image": {"src":"/test.png"}, "id":123456, "url":"/test.mp3"}]}]
+[{"id":"12345", "facebook_id":"122102047148149148", "image": {"url": "/test.png"}, "artist_name":"Retrofoxxo", "biography":{"text":"I'm a fox"}, "tracks":[{"title":"Foxxo & Friends", "purchase_url":"/purchase", "cover_image": {"src":"/test.png"}, "id":123456, "url":"/test.mp3"}]}]
