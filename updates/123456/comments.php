876e77e0e3e5aaf61a884df27cb4bd63af895950
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-24 15:38:43 +0100

    Add support for comments

--- updates/123456/comments.php
+++ updates/123456/comments.php
@@ -2,4 +2,4 @@
 header('Content-Type: application/json');
 header('Pagination: 1');
 ?>
-[]
+{"id": 12345637, "user": {"id": 12345, "facebook_id": "122102047148149148", "name": "Retrofoxxo", "last_name": "test"}, "comment": "Post comments test"}
