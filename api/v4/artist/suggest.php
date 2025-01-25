51e568807a018669c6138d3e3e5b3f0ff6ef6224
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-25 13:02:09 +0100

    Search Suggestions

---
+++ api/v4/artist/suggest.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type text/javascript');
+?>
+<?php echo $_GET['callback']; ?>({"response": {"status": {"version": "4.2", "code": 0, "message": "Success"}, "artists": [{"name": "Hot Chelle Rae", "id": "ARWIGPP11E8F5C02F8"}, {"name": "Red Hot Chili Peppers", "id": "ARE8GLF1187FB52532"}, {"name": "Chickenfoot", "id": "ARAXVBB12086C17781"}, {"name": "Chris Isaak", "id": "ARCZC791187B991DF4"}]}})
