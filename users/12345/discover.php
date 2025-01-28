940d211dac1f7962cb27dec7ca8426be18bfbba6
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-28 19:19:30 +0100

    Delete users directory
    Commited in wrong repo

--- users/12345/discover.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-header('Pagination: 1');
-?>
-[{"id":"12345", "artist_facebook_id":"122102047148149148", "artist_name":"Retrofoxxo"}]
--- users/12345/s3config.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json');
-?>
-{"url":"/upload", "artist_id":12345, "data":{}, "status":"OK"}
