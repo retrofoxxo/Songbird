c4a0ecb763ca3c7485d56ef35963e34e45678780
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-28 19:37:36 +0100

    Sync
    And figure out uploads later, it's so confusing, not even the javascript gives hints this time unfortunately

--- users/12345/s3config.php
+++ users/12345/s3config.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: application/json');
 ?>
-{"url":"/upload", "artist_id":12345, "data":{}, "status":"OK"}
+{"url":"/upload", "artist_id":12345, "data":{"id":12345}, "status":"ok"}
