1e14fe5445da5f8a19e0e9603e7ff2088c382480
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-29 15:51:21 +0100

    Implement followed artists

---
+++ users/12345/collected.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: application/json');
+header('Pagination: 1');
+?>
+[{"id":"12345", "facebook_id":"122102047148149148", "image": {"url": "/test.png"}, "artist_name":"Retrofoxxo"}]
