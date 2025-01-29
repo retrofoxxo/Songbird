a24d0138052dcbe2583a72613e79a62baaecedbe
Retrofoxxo <144725203+retrofoxxo@users.noreply.github.com>
2025-01-29 17:15:50 +0100

    add friends, collections and liked posts

--- users/12345/friends.php
+++ users/12345/friends.php
@@ -2,4 +2,4 @@
 header('Content-Type: application/json');
 header('Pagination: 1');
 ?>
-[]
+[{"id":"12345", "name":"Retrofoxxo", "like_count": 1, "comment_count": 1, "upload_count": 1, "last_name":"test", "facebook_id":"122102047148149148"}]
--- users/12345/likes.php
+++ users/12345/likes.php
@@ -1,4 +1,5 @@
 <?php
 header('Content-Type: application/json');
+header('Pagination: 1');
 ?>
-[]
+[{"title": "test", "id": 123456, "facebook_id":"122102047148149148", "description": "testing video's :>", "comment_count": 1, "like_count": 1, "liked": 0, "comments": [{"id": 1234567, "user": {"id": 12345, "facebook_id": "122102047148149148", "name": "Retrofoxxo", "last_name": "test"}, "comment": "testing comments"}], "cover_image": {"src": "/test.png", "height": 100}, "artist":{"artist_name": "zilch", "id": 123456}, "type": "video", "url": "https://www.youtube.com/watch?v=nDvrAyj9x0o"}]
